636587c91c00e92e685554d927211b41a9c8dbb9
Arkadiy <pv487732@gmail.com>
2024-11-23 21:26:27 +0300

    fix scenelevel1

--- Assets/Scripts/Room/RoomTrigger.cs
+++ Assets/Scripts/Room/RoomTrigger.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Room
@@ -5,11 +6,23 @@ namespace Room
     public class RoomTrigger : MonoBehaviour
     {
         [SerializeField] private Enemy _enemy;
+        [SerializeField] private int _secondsToActiveEnemy;
 
+        private bool _isActive;
+        private Coroutine _cor;
+        
         private void OnTriggerEnter(Collider other)
         {
-            if(other.CompareTag("Player"))
-                _enemy.gameObject.SetActive(true);
+            if(_cor is not null || _isActive)
+                return;
+            if (other.CompareTag("Player"))
+                _cor = StartCoroutine(TimerToActive());
+        }
+
+        private IEnumerator TimerToActive()
+        {
+            yield return new WaitForSeconds(_secondsToActiveEnemy);
+            _enemy.gameObject.SetActive(true);
         }
     }
 }
