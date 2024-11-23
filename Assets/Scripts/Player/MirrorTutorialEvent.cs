e9e7bf1fabdeb20e27db201c55366d0b5920fc67
Kapl <ivan_titov_2000@bk.ru>
2024-11-23 20:18:31 +0300

    Changed Level1Scene

--- Assets/Scripts/Player/MirrorTutorialEvent.cs
+++ Assets/Scripts/Player/MirrorTutorialEvent.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MirrorTutorialEvent : MonoBehaviour
 {
     [SerializeField] private bool _isTutor;
+    [SerializeField] private LayerMask ignoreMask;
     private bool _hit;
     
     private void Update()
@@ -10,8 +11,12 @@ public class MirrorTutorialEvent : MonoBehaviour
         if(!_isTutor)
             return;
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit,  LayerMask.NameToLayer("Enemy")))
+        int layerMask = ~(1 << ignoreMask);
+        if (Physics.Raycast(transform.position, transform.forward, out hit,Mathf.Infinity,layerMask))
         {
+            print(hit.transform.tag);
+            if(!hit.transform.CompareTag("Enemy"))
+                return;
             if (!_hit)
             {
                 hit.transform.TryGetComponent(out Enemy enemy);
