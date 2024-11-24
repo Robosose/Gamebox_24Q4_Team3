68abdaf456982a821b397a966c05902a0644a2ea
Kapl <ivan_titov_2000@bk.ru>
2024-11-24 16:19:57 +0300

    Added open corridor door

--- Assets/Scripts/InfinityRoom/InfinityRoom_DoorMovement.cs
+++ Assets/Scripts/InfinityRoom/InfinityRoom_DoorMovement.cs
@@ -6,7 +6,8 @@ public class InfinityRoom_DoorMovement : MonoBehaviour
     [SerializeField] private Transform _doorTransform;
     [SerializeField] private Transform[] _points;
     [SerializeField] private float _timeBeforeNextPoint;
-
+    [SerializeField] private GameObject door;
+    
     private int _pointIndex;
 
     private void OnTriggerEnter(Collider other)
@@ -23,14 +24,21 @@ public class InfinityRoom_DoorMovement : MonoBehaviour
     private IEnumerator MoveDoor()
     {
         var startTransform = _doorTransform.position;
-        var f = 0f;
-        while (f < 1f)
+        var t = 0f;
+        while (t < 1f)
         {
-            f += Time.deltaTime / _timeBeforeNextPoint;
-            _doorTransform.position = Vector3.Lerp(startTransform, _points[_pointIndex].position, f);
+            t += Time.deltaTime / _timeBeforeNextPoint;
+            _doorTransform.position = Vector3.Lerp(startTransform, _points[_pointIndex].position, t);
+            
+            if (_pointIndex + 1 == _points.Length)
+            {
+                door.transform.localRotation = Quaternion.Euler(door.transform.localRotation.x, Mathf.Lerp(0, 90, t),
+                    door.transform.localRotation.z);
+            }
             yield return null;
         }
-
+        
         _pointIndex++;
     }
+
 }
