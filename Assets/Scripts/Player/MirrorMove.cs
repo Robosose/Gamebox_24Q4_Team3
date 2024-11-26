c34186312f4fb65a010bad34bef54955736e1c17
Kapl <ivan_titov_2000@bk.ru>
2024-11-26 21:16:54 +0300

    Added wall borders for hand

--- Assets/Scripts/Player/MirrorMove.cs
+++ Assets/Scripts/Player/MirrorMove.cs
@@ -17,6 +17,8 @@ public class MirrorMove : MonoBehaviour
     [SerializeField] private float _sensitivity;
     [SerializeField] private float _crouchTime;
     [SerializeField] private PlayerControllers _playerControllers;
+    [SerializeField] private LayerMask _collisionLayers;
+
     private InputManager _inputManager;
     private float _rotateX;
     private float _rotateY;
@@ -24,6 +26,7 @@ public class MirrorMove : MonoBehaviour
     private float _baseRotateY;
     private float _mirrorMove;
     private float _crouchValue;
+    private float _handDistance;
 
     [Inject]
     private void Construct(InputManager inputManager)
@@ -33,23 +36,42 @@ public class MirrorMove : MonoBehaviour
         _baseRotateY = _rightHandTarget.localEulerAngles.y;
     }
 
+    private void Start()
+    {
+        _handDistance = GetFarthestDistance(_upperPointPosition, _upperCrouchPointPosition, _bottomPointPosition,
+            _bottomCrouchPointPosition);
+        print(_handDistance);
+    }
+
     private void Update()
     {
+        GetHand();
+        HandRotate();
+    }
+
+    private void GetHand()
+    {
         _mirrorMove = _inputManager.IsUsingMirror()
-             ? Mathf.Clamp01(_mirrorMove + Time.deltaTime)
-             : Mathf.Clamp01(_mirrorMove - Time.deltaTime);
+            ? Mathf.Clamp01(_mirrorMove + Time.deltaTime)
+            : Mathf.Clamp01(_mirrorMove - Time.deltaTime);
 
         _crouchValue = _inputManager.IsCrouching()
             ? Mathf.Clamp01(_crouchValue + Time.deltaTime / _crouchTime)
             : Mathf.Clamp01(_crouchValue - Time.deltaTime / _crouchTime);
 
         _rig.weight = _mirrorMove;
+
         var moveValue = Vector3.Lerp(
             Vector3.Lerp(_bottomPointPosition.localPosition, _bottomCrouchPointPosition.localPosition, _crouchValue),
             Vector3.Lerp(_upperPointPosition.localPosition, _upperCrouchPointPosition.localPosition, _crouchValue),
-            MapValueToZeroToOne(_camera.transform.localEulerAngles.x, _playerControllers.BottomLimit1, _playerControllers.UpperLimit1));
+            MapValueToZeroToOne(_camera.transform.localEulerAngles.x, _playerControllers.BottomLimit1,
+                _playerControllers.UpperLimit1));
 
-        _rightHandTarget.localPosition = moveValue;
+        _rightHandTarget.localPosition = CheckForCollision(transform.position, moveValue);
+    }
+
+    private void HandRotate()
+    {
         if (_inputManager.IsRotatingMirror())
         {
             _rotateX = Mathf.Clamp(_rotateX + _inputManager.GetMouseDelta().x * Time.deltaTime * _sensitivity,
@@ -61,6 +83,41 @@ public class MirrorMove : MonoBehaviour
 
             _rightHandTarget.localEulerAngles = new Vector3(_rotateY, -_rotateX, _rightHandTarget.localEulerAngles.z);
         }
+    }
+
+    private Vector3 CheckForCollision(Vector3 currentPosition, Vector3 targetPosition)
+    {
+        var direction = new Vector3(targetPosition.x, 0, targetPosition.z) -
+                        new Vector3(currentPosition.x, 0, currentPosition.z);
+        Debug.DrawRay(currentPosition, transform.forward.normalized * _handDistance, Color.blue, 1);
+
+        if (Physics.Raycast(currentPosition, transform.forward.normalized, out RaycastHit hit, _handDistance,
+                _collisionLayers))
+        {
+            float hitInfoDistance = _handDistance - hit.distance;
+            return targetPosition - direction.normalized * hitInfoDistance;
+        }
+
+        return targetPosition;
+    }
+
+    private float GetFarthestDistance(Transform point1, Transform point2, Transform point3, Transform point4)
+    {
+        Vector3[] points = { point1.localPosition, point2.localPosition, point3.localPosition, point4.localPosition };
+        float maxDistance = 0;
+
+        foreach (var point in points)
+        {
+            float distance = Mathf.Pow(point.x, 2) + Mathf.Pow(point.z, 2);
+            float distanceSqrt = Mathf.Sqrt(distance);
+
+            if (distanceSqrt > maxDistance)
+            {
+                maxDistance = distanceSqrt;
+            }
+        }
+
+        return maxDistance;
     }
 
     float MapValueToZeroToOne(float x, float minIn, float maxIn)
--- Assets/Scripts/Player/MirrorTutorialEvent.cs
+++ Assets/Scripts/Player/MirrorTutorialEvent.cs
@@ -32,10 +32,4 @@ public class MirrorTutorialEvent : MonoBehaviour
             }
         }
     }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(transform.position,transform.forward * 50f);
-    }
 }
