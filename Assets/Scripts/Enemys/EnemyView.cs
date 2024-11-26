e3575e79f737d9750d21a1a0ce8102ba2b35894f
Arkadiy <pv487732@gmail.com>
2024-11-26 23:16:40 +0300

    1

--- Assets/Scripts/Enemys/EnemyView.cs
+++ Assets/Scripts/Enemys/EnemyView.cs
@@ -5,10 +5,15 @@ namespace Enemys
     public class EnemyView : MonoBehaviour
     {
         [SerializeField] private Animator _animator;
+        [SerializeField] private AudioClip[] _clips;
+        
         private static readonly int IsRunning = Animator.StringToHash("IsRunning");
         private static readonly int IsWalking = Animator.StringToHash("IsWalking");
 
-        public void StartRunning() => _animator.SetBool(IsRunning, true);
+        public void StartRunning()
+        {
+            _animator.SetBool(IsRunning, true);
+        }
         public void StopRunning() => _animator.SetBool(IsRunning, false);
         public void StartWalking() => _animator.SetBool(IsWalking, true);
         public void StopWalking() => _animator.SetBool(IsWalking, false);
