edfe5678147e9c3920a4edc4041295a861ae3d77
Kapl <ivan_titov_2000@bk.ru>
2024-11-30 15:00:01 +0300

    Fixed audio coridor

--- Assets/Scripts/InfinityRoom/AudioTrigger.cs
+++ Assets/Scripts/InfinityRoom/AudioTrigger.cs
@@ -6,15 +6,19 @@ public class AudioTrigger : MonoBehaviour
     [SerializeField] private Transform door;
     private AudioSource _audioSource;
     private bool _isPlaying;
+    
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+        _audioSource.Play();
+        _audioSource.Pause();
     }
 
     private void Update()
     {
         if(_isPlaying)
             return;
+        
         if (Vector3.Distance(transform.position, door.position) < distance)
         {
             _audioSource.Play();
