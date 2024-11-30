afc0c29a66e3984cdab1cc34d3770fe8fe7413b7
Kapl <ivan_titov_2000@bk.ru>
2024-12-01 01:48:14 +0300

    Fixed cutscene

--- Assets/Scripts/Triggers/CutsceneTrigger.cs
+++ Assets/Scripts/Triggers/CutsceneTrigger.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 using UnityEngine.Video;
+using Zenject;
 
 public class CutsceneTrigger : MonoBehaviour
 {
     [SerializeField] private VideoPlayer _videoPlayer;
     [SerializeField] private AudioSource _audioSource;
-    [SerializeField] private GameObject _sounds;
+    private InputManager _inputManager;
+
+    [Inject]
+    private void Construct(InputManager inputManager)
+    {
+        _inputManager = inputManager;
+    }
+
+    private void OnEnable()
+    {
+        _videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    private void OnDisable()
+    {
+        _videoPlayer.loopPointReached -= OnVideoEnd;
+    }
+
+    private void OnVideoEnd(VideoPlayer source)
+    {
+        SceneSaver.Instance.LoadScene(ScenesType.MainMenuScene);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            _sounds.SetActive(false);
+            Sound.Instance.MuteMusicAndSound();
+            _inputManager.ActionDisable();
             Time.timeScale = 0;
             _videoPlayer.Play();
             _audioSource.Play();
