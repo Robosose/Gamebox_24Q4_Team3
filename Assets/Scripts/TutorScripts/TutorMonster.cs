864564951d69699d95b2e05c498bc8633efafae8
Kapl <ivan_titov_2000@bk.ru>
2024-11-30 09:20:08 +0300

    Fixed bug tutor collider

--- Assets/Scripts/TutorScripts/TutorMonster.cs
+++ Assets/Scripts/TutorScripts/TutorMonster.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class TutorMonster : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject playerMirror;
     [SerializeField] private AudioSource _audioSourceStep;
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!playerMirror.activeSelf)
+            return;
         if (other.CompareTag("Player") )
         {
             _audioSourceStep.Stop();
